425911f6b538d754d6075b73adeb8d3a54514be0
Karan Shah <64479353+karanshah-browserstack@users.noreply.github.com>
2023-02-23 22:55:29 +0530

    Update DriverManager to use yml

--- src/test/java/com/browserstack/stepdefs/DriverManager.java
+++ src/test/java/com/browserstack/stepdefs/DriverManager.java
@@ -6,23 +6,42 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.File;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.HashMap;
+import java.util.Map;
 
 public class DriverManager {
 
+    public static final String USER_DIR = "user.dir";
+    public static String userName, accessKey;
+    public static Map<String, Object> browserStackYamlMap;
     private static ThreadLocal<WebDriver> driverManager = new ThreadLocal<>();
+
+    public DriverManager() {
+        File file = new File(getUserDir() + "/browserstack.yml");
+        this.browserStackYamlMap = convertYamlFileToMap(file, new HashMap<>());
+    }
+
+    public static WebDriver getDriver() {
+        return driverManager.get();
+    }
 
     @Before
     public void setUp() throws MalformedURLException {
         MutableCapabilities capabilities = new MutableCapabilities();
         HashMap<String, String> bstackOptions = new HashMap<>();
+        userName = System.getenv("BROWSERSTACK_USERNAME") != null ? System.getenv("BROWSERSTACK_USERNAME") : (String) browserStackYamlMap.get("userName");
+        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY") != null ? System.getenv("BROWSERSTACK_ACCESS_KEY") : (String) browserStackYamlMap.get("accessKey");
         bstackOptions.putIfAbsent("source", "cucumber-java:sample-sdk:v1.0");
         capabilities.setCapability("bstack:options", bstackOptions);
         driverManager.set(new RemoteWebDriver(
-            new URL("https://hub.browserstack.com/wd/hub"), capabilities));
+                new URL(String.format("https://%s:%s@hub.browserstack.com/wd/hub", userName , accessKey)), capabilities));
     }
 
     @After
@@ -31,7 +50,19 @@ public class DriverManager {
         driverManager.get().quit();
     }
 
-    public static WebDriver getDriver() {
-        return driverManager.get();
+    private String getUserDir() {
+        return System.getProperty(USER_DIR);
+    }
+
+    private Map<String, Object> convertYamlFileToMap(File yamlFile, Map<String, Object> map) {
+        try {
+            InputStream inputStream = Files.newInputStream(yamlFile.toPath());
+            Yaml yaml = new Yaml();
+            Map<String, Object> config = yaml.load(inputStream);
+            map.putAll(config);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Malformed browserstack.yml file - %s.", e));
+        }
+        return map;
     }
 }
